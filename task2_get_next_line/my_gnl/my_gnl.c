06fd81407ea8ed34bdea2ff58736a62a8d7f38a5
vitt0ri0 <vitt0ri0.progr@gmail.com>
2020-07-27 21:26:05 +0300

    GNL_TIME basic working version

--- task2_get_next_line/my_gnl/my_gnl.c
+++ task2_get_next_line/my_gnl/my_gnl.c
@@ -3,18 +3,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define buf_size 10
+#define buf_size 3
 static char read_buf[buf_size + 1];
 static char *print_buf;
-static size_t *print_buf_size;
-
 static size_t pos = 0;
-static char *buf2;
 static int is_eof = 0;
 static int read_amount;
-static int new_line = 0;
-
-
+static int last_nl = -2;
 
 size_t	ft_linelen(const char *s)
 {
@@ -26,37 +21,32 @@ size_t	ft_linelen(const char *s)
 	return (i);
 }
 
-size_t check_eof(const char *s, size_t pos)
-{
-	if (s[pos] == EOF)
-		return 1;
-	return 0;
-}
-
-char		*ft_linedup(const char *src, size_t pos_finish)
+char		*ft_linedup(const char *src, int start_pos, int pos_finish)
 {
 	size_t		len;
 	char	*new;
 
-	len = ft_linelen(src);
+	len = ft_linelen(src + start_pos);
 	if (len > pos_finish)
 		len = pos_finish;
 
 	new = malloc(sizeof(char) * (len + 1));
 	if (!new)
 		return (NULL);
-	ft_strlcpy(new, src, len + 1);
+	ft_strlcpy(new, src + start_pos, len + 1);
 
 	return (new);
 }
 
 
-size_t	ft_strchr_pos(const char *s, char c, size_t pos_start)
+int	ft_strchr_pos(const char *s, char c, size_t pos_start)
 {
 	int		i;
 	int		len;
 
 	i = pos_start;
+	if (!s)
+		return (NO_NL);
 	len = ft_strlen(s);
 
 	while (i <= len)
@@ -68,7 +58,7 @@ size_t	ft_strchr_pos(const char *s, char c, size_t pos_start)
 	return (NO_NL);
 }
 
-char	*ft_strjoin_s2(char *s1, const char *s2, size_t start_pos, size_t finish_pos)
+char	*ft_strjoin_s2(char *s1, const char *s2, int start_pos, int finish_pos)
 {
 	int		i;
 	int		j;
@@ -78,15 +68,20 @@ char	*ft_strjoin_s2(char *s1, const char *s2, size_t start_pos, size_t finish_po
 	if (!s1 || !s2)
 		return (NULL);
 	i = 0;
-	j = 0;
-	len = ft_strlen(s1) + ft_strlen(s2);
+	j = start_pos;
+
+	len = ft_strlen(s2);
+	if (len > finish_pos)
+		len = finish_pos;
+	len += ft_strlen(s1 + start_pos);
+
 	ss = (char*)malloc(sizeof(char) * (len + 1));
 	if (!ss)
 		return (NULL);
 	while (s1[j] != TERM)
 		ss[i++] = s1[j++];
-	j = start_pos;
-	while (s2[j] != TERM || j == finish_pos)
+	j = 0;
+	while (s2[j] != TERM && j < finish_pos)
 	{
 		ss[i] = s2[j++];
 		i++;
@@ -95,49 +90,45 @@ char	*ft_strjoin_s2(char *s1, const char *s2, size_t start_pos, size_t finish_po
 
 	free(s1);
 
-	s1 = ss;
-
-	return s1;
+	return ss;
 }
 
 int get_next_line(int fd, char **line)
 {
-	int last_nl = ft_strchr_pos(print_buf, NL, pos);
-
-	while (last_nl == NO_NL)
+	if (last_nl == -2)
+	{
+		read_buf[buf_size] = TERM;
+		print_buf = ft_strdup("");
+		read_buf[0] = TERM;
+	}
+	last_nl = ft_strchr_pos(print_buf, NL, pos);
+	while (last_nl == NO_NL && !is_eof)
 	{
 		read_amount = read(fd, read_buf, buf_size);
-
-		print_buf = ft_strjoin_s2(print_buf, read_buf, pos, last_nl);
-
-		last_nl = ft_strchr_pos(read_buf, NL, last_nl + 1);
+		if (read_amount < buf_size)
+			is_eof = 1;
+		print_buf = ft_strjoin_s2(print_buf, read_buf, pos, read_amount);
+		last_nl = ft_strchr_pos(print_buf, NL, last_nl+1);
+		pos = 0;
 	}
 
-	*line = ft_linedup(print_buf, last_nl);
+	if (is_eof && last_nl == -1)
+		return -1;
+	*line = ft_linedup(print_buf, pos, last_nl);
 	pos = last_nl + 1;
-
 	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	int fd = open("test.txt", O_RDONLY);
-	if (fd < 0){
+	int fd = open("test2.txt", O_RDONLY);
+	if (fd < 0)
 		return (1);
-	}
 	char *line;
 
-	read_buf[buf_size] = TERM;
-	buf2[0] = TERM;
-	read_buf[0] = TERM;
-
-	// todo debug
-	read_amount = read(fd, read_buf, buf_size);
-
-	while (read_amount > 0)
+	while (get_next_line(fd, &line) >= 0)
 	{
-		get_next_line(fd, &line);
 		ft_putstr_fd(line, 0);
-		ft_putstr_fd("\n", 0);
+		ft_putstr_fd("_\n", 0);
 	}
 }
